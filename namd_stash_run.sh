1fc496db3ef238eb6bb9523d5208863c11a80f91
bala <dmbala@gmail.com>
2014-12-01 15:03:12 -0600

    revert to grid

--- namd_stash_run.sh
+++ namd_stash_run.sh
@@ -2,4 +2,8 @@
 source /cvmfs/oasis.opensciencegrid.org/osg/modules/lmod/5.6.2/init/bash
 module load namd/2.9
 wget --no-check-certificate http://stash.osgconnect.net/+dbala/Namd_param/par_all27_prot_lipid.inp
-namd2 ubq_gbis_eq.conf 
+namd2 ubq_gbis_eq.conf > ubq_gbis_eq.log
+mkdir -p OutFilesFromNAMD
+cp * OutFilesFromNAMD/.
+tar cvzf OutFilesFromNAMD.tar.gz OutFilesFromNAMD
+
